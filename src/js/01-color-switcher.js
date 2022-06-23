bb3cc82c1aa363b6c87ed78e25df987e13c377d8
Anton <anton_surkov@rambler.ru>
2022-06-23 23:52:08 +0300

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+refs.stop.disabled = true;
+
+let intervalId = 0;
+
+refs.start.addEventListener('click', e => {
+  e.target.disabled = true;
+  refs.stop.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', e => {
+  clearInterval(intervalId);
+
+  e.target.disabled = true;
+  refs.start.disabled = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
