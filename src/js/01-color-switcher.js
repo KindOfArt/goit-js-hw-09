6aad65b3a2787f0888796e55ed89507ef87d92e7
Anton <anton_surkov@rambler.ru>
2022-06-24 17:42:15 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const refs = {
   start: document.querySelector('[data-start]'),
   stop: document.querySelector('[data-stop]'),
+  body: document.body,
 };
 
 refs.stop.disabled = true;
@@ -9,10 +10,13 @@ let intervalId = 0;
 
 refs.start.addEventListener('click', e => {
   e.target.disabled = true;
+
   refs.stop.disabled = false;
 
+  colorSwitcher(refs.body);
+
   intervalId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
+    colorSwitcher(refs.body);
   }, 1000);
 });
 
@@ -25,4 +29,8 @@ refs.stop.addEventListener('click', e => {
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorSwitcher(domElement) {
+  domElement.style.backgroundColor = getRandomHexColor();
 }
